7c12550fbbc083f817ba436cf512b211c7704cc0
WebMobik <ddimon3000@bk.ru>
2020-01-22 19:14:56 +0300

    Updates

---
+++ precache-manifest.fa29753129c2a78f7990a21a1565d85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fba5ec0e875644ca2fa23f46398103",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "db2fec081379518ed97a",
+    "url": "/pokedex/static/css/main.f12ca001.chunk.css"
+  },
+  {
+    "revision": "a74b67116921ab10fc53",
+    "url": "/pokedex/static/js/2.39158a49.chunk.js"
+  },
+  {
+    "revision": "db2fec081379518ed97a",
+    "url": "/pokedex/static/js/main.a4b6913a.chunk.js"
+  },
+  {
+    "revision": "c4237ef3e07ed4b9e442",
+    "url": "/pokedex/static/js/runtime-main.4c5636af.js"
+  },
+  {
+    "revision": "058cc1913cf7d2bd93d6153ad22205f5",
+    "url": "/pokedex/static/media/pokeball_PNG24.058cc191.webp"
+  }
+]);
